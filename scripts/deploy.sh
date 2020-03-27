c625079e468d6eadfe831e795038d0c0ca8c7e82
Melania Dello Spedale La Paglia <melania.dellospedalelapaglia@netsenseweb.com>
2020-03-27 15:31:46 +0100

    aggiornato script deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,9 @@ npm run build
 cd ~/DNSServerApp/backend;
 npm install
 npm run be:build
+cd ~/DNSServerApp/frontend
+npm install
+npm run build
 cd ~/DNSServerApp/backend
 NODE_ENV=staging pm2 start dist/main.js --name "dnsserverapp" && cd ~/ && pm2 startup > pm2_startup_output;
 tail -n 1 pm2_startup_output > pm2_startup.sh && chmod u+rwx pm2_startup.sh && ./pm2_startup.sh && pm2 save
