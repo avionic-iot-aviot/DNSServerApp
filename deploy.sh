6a973ca941c8d29a47bcfcc01790f1200ecc44a3
Andrea Santangelo <asantangelo90@gmail.com>
2021-01-14 15:09:27 +0100

    Added Dockerfile and deploy.sh.

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+PROJECT_FOLDER="DNSServerApp"
+
+echo 'Scarico da Github'
+rm -rf $PROJECT_FOLDER
+git clone https://github.com/avionic-iot-aviot/DNSServerApp
+
+echo 'Avvio di deploy'
+
+pm2 delete 0
+cd ~/$PROJECT_FOLDER/backend
+npm install
+npm run be:build
+cd ~/$PROJECT_FOLDER/backend
+cat << EOF > ecosystem.config.js
+module.exports = {
+  apps : [
+      {
+        name: "dnsserverapp",
+        script: "./dist/main.js",
+        watch: true,
+        instance_var: 'INSTANCE_ID',
+        env: {
+            "NODE_ENV": "staging"
+        }
+      }
+  ]
+}
+EOF
+pm2 start ecosystem.config.js && cd ~/ && pm2 startup openrc > pm2_startup_output &&
+tail -n 1 pm2_startup_output > pm2_startup.sh && chmod a+rwx pm2_startup.sh && ./pm2_startup.sh && pm2 save
+pm2 stop 0
+pm2 start 0 & pm2 logs 0
+
